3c8ad9393278cfaba14a459ee06633b5d0bf2660
marelas <marela.as@gmail.com>
2023-04-19 18:16:00 -0300

    Updating spring securty

--- src/main/java/ar/security/configuration/SecurityConfiguration.java
+++ src/main/java/ar/security/configuration/SecurityConfiguration.java
@@ -1,35 +1,45 @@
 package ar.security.configuration;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfiguration  extends WebSecurityConfigurerAdapter{
-	
-	@Autowired
-	CustomSuccessHandler customSuccessHandler;
-	
-	@Autowired
-	public void configureGlobalSecurity(AuthenticationManagerBuilder auth) throws Exception{
-		auth.inMemoryAuthentication().withUser("marian").password("1234").roles("USER");
-		auth.inMemoryAuthentication().withUser("admin").password("1111").roles("ADMIN");
-		auth.inMemoryAuthentication().withUser("dba").password("2222").roles("ADMIN","DBA");
+public class SecurityConfiguration {
+
+	/** As this is an example we will set up users in memory,
+	 *  not safe for production */
+	@Bean
+	public InMemoryUserDetailsManager userDetailsManager() {
+		UserDetails user = User.withDefaultPasswordEncoder()
+						.username("marian").password("12345").roles("USER").build();
+		UserDetails admin = User.withDefaultPasswordEncoder()
+						.username("admin").password("11111").roles("ADMIN").build();
+		UserDetails dba = User.withDefaultPasswordEncoder()
+						.username("dbadmin").password("22222").roles("DBA").build();
+		return new InMemoryUserDetailsManager(user, admin, dba);
 	}
-	
-	protected void configure(HttpSecurity http) throws Exception{
-		http.authorizeRequests()
-			.antMatchers("/", "/home").access("hasRole('USER')")
-			.antMatchers("/admin/**").access("hasRole('ADMIN')")
-			.antMatchers("/dba/**").access("hasRole('DBA') and hasRole('DBA')")
-			.and().formLogin().loginPage("/login").successHandler(customSuccessHandler)
-			.usernameParameter("ssoId").passwordParameter("password")
-			.and().csrf()
-			.and().exceptionHandling().accessDeniedPage("/access_denied");
+
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+
+		return http
+				.csrf().disable()
+				.authorizeHttpRequests((authz) -> authz
+								.requestMatchers("/").permitAll()
+								.requestMatchers("/", "/home").hasRole("USER")
+								.requestMatchers("/admin/**", "/dba/**").hasRole("ADMIN")
+								.requestMatchers("/dba/**").hasRole("DBA")
+								.anyRequest().authenticated())
+				.formLogin(Customizer.withDefaults())
+				.sessionManagement((sessionMngmt) -> sessionMngmt.maximumSessions(1)).build();
 	}
 
 	
--- src/main/java/ar/security/controller/HelloWorldController.java
+++ src/main/java/ar/security/controller/HelloWorldController.java
@@ -1,68 +1,24 @@
 package ar.security.controller;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
-
-@Controller
+@RestController
 public class HelloWorldController {
 	
-	@RequestMapping(value={"/", "home"}, method = RequestMethod.GET)
-	public String homePage(ModelMap model){
-		model.addAttribute("user", getPrincipal());
-		return "welcome";
+	@GetMapping("/home")
+	public String homePage(){
+		return "Hello";
 	}
 	
-	@RequestMapping(value="/admin", method=RequestMethod.GET)
-	public String adminPage(ModelMap model){
-		model.addAttribute("user", getPrincipal());
-		return "admin";
+	@GetMapping("/admin")
+	public String adminPage(){
+		return "Hello Admin";
 	}
 	
-	@RequestMapping(value="/dba", method= RequestMethod.GET)
-	public String dbaPage(ModelMap model){
-		model.addAttribute("user", getPrincipal());
-		return "dba";
+	@GetMapping(value="/dba")
+	public String dbaPage(){
+		return "Hello DBAdmin";
 	}
-	
-	@RequestMapping(value="/accessDenied", method= RequestMethod.GET)
-	public String accessDeniedPage(ModelMap model){
-		model.addAttribute("user", getPrincipal());
-		return "accessDenied";
-	}
-	
-	@RequestMapping(value="/logout", method= RequestMethod.GET)
-	public String logoutPage(HttpServletRequest request, HttpServletResponse response){
-		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-		if(auth != null){
-			new SecurityContextLogoutHandler().logout(request, response, auth);
-		}
-		return  "redirect:/login?logout";
-	}
-	
-	@RequestMapping(value="/login", method= RequestMethod.GET)
-	public String loginPage(ModelMap model){
-		return "login";
-	}
-	
-	private String getPrincipal(){
-		String userName = null;
-		Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-		
-		if(principal instanceof UserDetails){
-			userName = ((UserDetails)principal).getUsername();
-		}else{
-			userName = principal.toString();
-		}
-		return userName;
-	}
+
 }
